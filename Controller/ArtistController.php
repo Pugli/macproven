bf28e32fae7c1db86a3606fa81a9c74cba771d3e
ezemacchi <40012550+ezemacchi@users.noreply.github.com>
2018-10-06 11:43:07 -0300

    Add files via upload

--- Controller/ArtistController.php
+++ Controller/ArtistController.php
@@ -1,1 +1,18 @@
+<?php
+    namespace Controller;
 
+    class ArtistController{
+
+        public function newArtist($name)
+        {
+           if($this->isArtistName($name)){
+               //agrega
+           }
+           else{
+               //no agrega y retorna false o null
+           }
+        }
+
+        
+    }
+?>
