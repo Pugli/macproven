e670a9a349684eb3e1e98902f1b4a2cae1935de2
ezemacchi <40012550+ezemacchi@users.noreply.github.com>
2018-10-06 11:42:27 -0300

    Add files via upload

--- Config/Autoload.php
+++ Config/Autoload.php
@@ -1,1 +1,18 @@
+<?php
 
+    namespace Config;
+
+    class Autoload{
+
+        public function start()
+        {
+            define("ROOT", dirname(__DIR__));
+        
+            spl_autoload_register(function ($className)
+            {
+               require_once(ROOT."/".$className.".php");
+            });            
+        }
+    }
+        
+?>
