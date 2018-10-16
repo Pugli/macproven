2563855f8d390f2f264cfb48a958c397f181f18f
arianjoan <arianproven@gmail.com>
2018-10-16 18:31:38 -0300

    Categories
    Macchi eat dicks

---
+++ Views/CategoryList.php
@@ -0,0 +1,51 @@
+<?php namespace View;
+
+    use controllers\ControllerCategory as ControllerCategory;
+
+    $controller = new ControllerCategory();
+
+    $CategoryList = $controller->getAll();
+?>
+
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Listado de Categorias</h2>
+               <table class="table bg-light-alpha">
+                    <thead>
+                         <th>Id</th>
+                         <th>Tipo de evento</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                            if(isset($CategoryList))
+                            {
+                                foreach($CategoryList as $Category)
+                                {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $Category->getId() ?></td>
+                                            <td><?php echo $Category->getDescription() ?></td>
+                                        </tr>
+                                    <?php
+                                }
+                            }
+                        ?>
+                    </tbody>
+               </table>
+          </div>
+     </section>
+
+     <section id="eliminar">
+          <div class="container">
+               <h2 class="mb-4">Eliminar Categoria</h2>
+
+               <form method="post" action="<?php echo FRONT_ROOT ?>Category/delete" class="form-inline bg-light-alpha p-5">
+                    <div class="form-group text-white">
+                         <label for="">Id</label>
+                         <input type="text" name="idCategory" value="" class="form-control ml-3">
+                    </div>
+                    <button type="submit" name="button" class="btn btn-danger ml-3">Eliminar</button>
+               </form>
+          </div>
+     </section>
---
+++ dao/DaoCategoryPdo.php
@@ -0,0 +1,108 @@
+<?php
+    namespace dao;
+    
+    use dao\IDaoCategory as IDaoCategory;
+    use Model\Category as Category;
+    use \Exception as Exception;
+    use Dao\Connection as Connection;
+
+    class DaoCategoryPdo implements IDaoCategory
+    {
+        private $connection;
+        private $tableName = "categories";
+
+
+        public function add(Category $category)
+        {
+            try
+            {
+                $query = "INSERT INTO ".$this->tableName." (category) VALUES (:category);";
+                $parameters["category"] = $category->getDescription();
+
+                $this->connection = Connection::GetInstance();
+
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function GetAll()
+        {
+            try
+            {
+                $categoryList = array();
+
+                $query = "SELECT * FROM ".$this->tableName;
+
+                $this->connection = Connection::GetInstance();
+
+                $resultSet = $this->connection->Execute($query);
+                
+                foreach ($resultSet as $row)
+                {                
+                    $category = new Category();
+                    $category->setDescription($row["category"]);
+                    $category->setId($row['id_category']);
+
+                    array_push($categoryList, $category);
+                }
+
+                return $categoryList;
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function checkCategory($categoryname)
+        {
+            try
+            {
+                $category = null;
+
+                $query = "SELECT * FROM ".$this->tableName." WHERE category = :category";
+
+                $parameters["category"] = $categoryname;
+
+                $this->connection = Connection::GetInstance();
+
+                $resultSet = $this->connection->Execute($query, $parameters);
+                
+                foreach ($resultSet as $row)
+                {
+                    $category = new Category();
+                    $category->setDescription($row["category"]);
+                    $category->setId($row["id_category"]);
+                }
+                            
+                return $category;
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function Delete($idCategory)
+        {
+            try
+            {
+                $query = "DELETE FROM ".$this->tableName." WHERE id_category = :idCategory";
+            
+                $parameters["idCategory"] = $idCategory;
+
+                $this->connection = Connection::GetInstance();
+
+                $this->connection->ExecuteNonQuery($query, $parameters);   
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }            
+        }
+    }
+?>
