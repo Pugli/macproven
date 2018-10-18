f4995aa0d892d4b6a0c4623a615b090095d0d14f
arianjoan <arianproven@gmail.com>
2018-10-18 19:36:04 -0300

    ASDASDAS

--- Views/home.php
+++ Views/home.php
@@ -4,4 +4,13 @@
 <div class="text-center">
     <input class="btn btn-success ml-3" type="submit" value="Lista de Artistas" onclick="location='<?php echo FRONT_ROOT?>Artist'">
     <input class="btn btn-success ml-3" type="submit" value="Añadir un Artista" onclick="location='<?php echo FRONT_ROOT?>Artist/showAddArtist'">
+    <br><br>
+    <input class="btn btn-success ml-3" type="submit" value="Lista de Categorias" onclick="location='<?php echo FRONT_ROOT?>Category'">
+    <input class="btn btn-success ml-3" type="submit" value="Añadir un Categoria" onclick="location='<?php echo FRONT_ROOT?>Category/showAddCategory'">
+    <br><br>
+    <input class="btn btn-success ml-3" type="submit" value="Lista de Eventos" onclick="location='<?php echo FRONT_ROOT?>Event'">
+    <input class="btn btn-success ml-3" type="submit" value="Añadir un Evento" onclick="location='<?php echo FRONT_ROOT?>Event/showAddEvent'">
+    <br><br>
+    <input class="btn btn-success ml-3" type="submit" value="Lista de Espacios" onclick="location='<?php echo FRONT_ROOT?>EventPlace'">
+    <input class="btn btn-success ml-3" type="submit" value="Añadir un Espacio" onclick="location='<?php echo FRONT_ROOT?>EventPlace/showAddEventPlace'">
 </div>
