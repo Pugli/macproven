f063e5c5f58665893e46b0b903cab58687df1f25
ezemacchi <eze_macchimallo@hotmail.com>
2018-11-17 17:36:55 -0300

    Add model user

---
+++ Model/User.php
@@ -0,0 +1,27 @@
+<?php
+    namespace Model;
+
+    class User
+    {
+        private $email;
+        private $password;
+
+        public function setEmail($email)
+        {
+            $this->email = $email;
+        }
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        public function setPassword($password)
+        {
+            $this->password = $password;
+        }
+        public function getPassword()
+        {
+            return $this->password;
+        }
+    }
+?>
