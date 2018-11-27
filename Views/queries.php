c4838c6de7b0b027fc0341087e999fdcd2aaf456
Agustin Gonzalo Pugliese <aguspugli@hotmail.com>
2018-11-27 19:07:11 -0300

    .

--- Views/queries.php
+++ Views/queries.php
@@ -4,6 +4,7 @@
     <h1>Consultar por categoria</h1>
 </head>
 <body>
+<div>
     <form action="<?php echo FRONT_ROOT; ?>/event/checkEventForCategory" method="POST">
     <br>
     <tr>
@@ -20,7 +21,10 @@
     </tr>
         
         <input type="submit" value="Enviar"/>
+
     </form>
+    </div>
+    <div>
   <h1>Consultar por fecha</h1>
     <form action="<?php echo FRONT_ROOT; ?>/event/checkEventForDate" method="POST">
     <br>
@@ -32,5 +36,26 @@
         
         <input type="submit" value="Enviar"/>
     </form>
+    </div>
+    <div>
+    <form action="<?php echo FRONT_ROOT; ?>/event/checkEventForArtist" method="POST">
+    <br>
+    <h1>Consultar por artista:</h1>
+    <tr>
+    
+        <td><select name="artist">
+                <?php 
+                foreach ($arrayArtist as $i){
+                    ?>
+                    <option value="<?php echo $i->getId()?>"><?php echo $i->getName()?></option>
+                <?php }?>
+            </select></td>
+        
+     
+    </tr>
+        
+        <input type="submit" value="Enviar"/>
+    </form>
+    </div>
 </body>
 </html>
